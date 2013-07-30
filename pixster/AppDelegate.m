d9a4f2756c64d2d08e8e00b514cb9124242942d4
Timothy Lee <timothy1ee@yahoo.com>
2013-07-30 12:07:40 -0700

    Initial implementation.

--- pixster/AppDelegate.m
+++ pixster/AppDelegate.m
@@ -7,13 +7,16 @@
 //
 
 #import "AppDelegate.h"
+#import "SearchViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[SearchViewController alloc] init]];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ pixster/SearchViewController.m
@@ -0,0 +1,125 @@
+//
+//  SearchViewController.m
+//  pixster
+//
+//  Created by Timothy Lee on 7/30/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import "SearchViewController.h"
+#import "UIImageView+AFNetworking.h"
+#import "AFNetworking.h"
+
+@interface SearchViewController ()
+
+@property (nonatomic, strong) NSMutableArray *imageResults;
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Pixster";
+        self.imageResults = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)aTableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)aTableView numberOfRowsInSection:(NSInteger)section {
+    return [self.imageResults count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"CellIdentifier";
+    
+    // Dequeue or create a cell of the appropriate type.
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    UIImageView *imageView = nil;
+    const int IMAGE_TAG = 1;
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        imageView.tag = IMAGE_TAG;
+        [cell.contentView addSubview:imageView];
+    } else {
+        imageView = (UIImageView *)[cell.contentView viewWithTag:IMAGE_TAG];
+    }
+    
+    // Clear the previous image
+    imageView.image = nil;
+    [imageView setImageWithURL:[NSURL URLWithString:[self.imageResults[indexPath.row] valueForKeyPath:@"url"]]];
+    
+    return cell;
+}
+
+- (float)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 300;
+}
+
+#pragma mark - UISearchDisplay delegate
+
+- (void)searchDisplayControllerDidBeginSearch:(UISearchDisplayController *)controller {
+    [self.imageResults removeAllObjects];
+    [self.searchDisplayController.searchResultsTableView reloadData];
+}
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    return NO;
+}
+
+#pragma mark - UISearchBar delegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar setShowsCancelButton:NO animated:YES];
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%@", [searchBar.text stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        id results = [JSON valueForKeyPath:@"responseData.results"];
+        if ([results isKindOfClass:[NSArray class]]) {
+            [self.imageResults removeAllObjects];
+            [self.imageResults addObjectsFromArray:results];
+            [self.searchDisplayController.searchResultsTableView reloadData];
+        }
+    } failure:nil];
+    
+    [operation start];
+}
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    [searchBar setShowsCancelButton:YES animated:YES];
+}
+
+- (BOOL)searchBarShouldEndEditing:(UISearchBar *)searchBar {
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    return YES;
+}
+
+@end
