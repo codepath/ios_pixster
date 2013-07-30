9723f2d1aeaddd7e95e5e8916523ef684bf5ba83
Timothy Lee <timothy1ee@yahoo.com>
2013-07-30 09:15:39 -0700

    Initial Commit

---
+++ pixster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pixster
+//
+//  Created by Timothy Lee on 7/30/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
